015b914ac6157b00104e0534fa55ea5c6eaf6264
Silver1063 <brickbrickerson1063@gmail.com>
2024-05-11 21:00:06 -0700

    Update history.js

--- routes/history.js
+++ routes/history.js
@@ -3,28 +3,25 @@ import { db } from '../server.js';
 
 const router = express.Router();
 
-// 1. Endpoint GET /history
-
-// This endpoint retrieves data from the search_history collection in MongoDB
-// This endpoint accepts and an optional query parameter:  searchTerm
-// If no query parameter is provided, returns a JSON response of all search history saved in the Atlas Cloud MongoDB.
-// If a query parameter is provided, returns a JSON response of the search history associated with the search term in the Atlas Cloud MongoDB.
-
 router.get('/', async (req, res) => {
     try {
         const { query } = req;
 
+        // Set the collection and create a filter for finding the document
         const collection = 'search_history';
         const filter = { searchTerm: query.searchTerm };
 
+        // let because mutable
         let cursor;
 
+        // if searchTerm not provided find everything, else use the filter
         if (query.searchTerm) {
             cursor = await db.find(collection, filter);
         } else {
             cursor = await db.find(collection);
         }
 
+        // turn the cursor into an array
         const result = await cursor.toArray();
 
         res.json(result);
